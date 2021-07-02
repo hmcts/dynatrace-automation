881989d6065c5047d7ec68896b5666a9591f16cc
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-07-02 10:29:38 +0100

    updated downloaded output file name

--- pipeline-scripts/install-monaco-cli.sh
+++ pipeline-scripts/install-monaco-cli.sh
@@ -4,5 +4,7 @@ CLI_PLATFORM="monaco-linux-amd64"
 CLI_VERSION="v1.6.0"
 DOWNLOAD_URL="https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/$CLI_VERSION/$CLI_PLATFORM"
 
-sudo curl -SL --retry 3 $DOWNLOAD_URL -o /usr/local/bin/monaco-linux
-sudo chmod +x /usr/local/bin/monaco-linux
+sudo curl -SL --retry 3 $DOWNLOAD_URL -o /usr/local/bin/monaco
+sudo chmod +x /usr/local/bin/monaco
+
+monaco
