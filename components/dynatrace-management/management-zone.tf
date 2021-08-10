914fb2b70d94768036297ca90328efbc2acfc530
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-08-10 07:13:52 +0100

    added rules

--- components/dynatrace-management/management-zone.tf
+++ components/dynatrace-management/management-zone.tf
@@ -13,6 +13,97 @@ resource "dynatrace_management_zone" "management-zone" {
         iterator = conditions
         for_each = rules.value.conditions
         content {
+          dynamic "application_type" {
+            iterator = application_type
+            for_each = { for k, v in conditions.value : k => v if k == "application_type" }
+            content {
+              operator       = application_type.value.operator
+              negate         = application_type.value.negate
+              value          = application_type.value.value
+            }
+          }
+          dynamic "application_type_comparison" {
+            iterator = application_type
+            for_each = { for k, v in conditions.value : k => v if k == "application_type_comparison" }
+            content {
+              operator       = application_type_comparison.value.operator
+              negate         = application_type_comparison.value.negate
+              type           = application_type_comparison.value.type
+              value          = application_type_comparison.value.value
+            }
+          }
+          dynamic "azure_compute_mode" {
+            iterator = azure_compute_mode
+            for_each = { for k, v in conditions.value : k => v if k == "azure_compute_mode" }
+            content {
+              operator       = azure_compute_mode.value.operator
+              negate         = azure_compute_mode.value.negate
+              value          = azure_compute_mode.value.value
+            }
+          }
+          dynamic "azure_compute_mode_comparison" {
+            iterator = azure_compute_mode_comparison
+            for_each = { for k, v in conditions.value : k => v if k == "azure_compute_mode_comparison" }
+            content {
+              operator       = azure_compute_mode_comparison.value.operator
+              negate         = azure_compute_mode_comparison.value.negate
+              value          = azure_compute_mode_comparison.value.value
+            }
+          }
+          dynamic "azure_sku" {
+            iterator = azure_sku
+            for_each = { for k, v in conditions.value : k => v if k == "azure_sku" }
+            content {
+              operator       = azure_sku.value.operator
+              negate         = azure_sku.value.negate
+              value          = azure_sku.value.value
+            }
+          }
+          dynamic "azure_sku_comparision" {
+            iterator = azure_sku_comparision
+            for_each = { for k, v in conditions.value : k => v if k == "azure_sku_comparision" }
+            content {
+              operator       = azure_sku_comparision.value.operator
+              negate         = azure_sku_comparision.value.negate
+              type           = azure_sku_comparision.value.type
+              value          = azure_sku_comparision.value.value
+            }
+          }
+          dynamic "base_comparison_basic" {
+            iterator = base_comparison_basic
+            for_each = { for k, v in conditions.value : k => v if k == "base_comparison_basic" }
+            content {
+              type           = base_comparison_basic.value.type
+              negate         = base_comparison_basic.value.negate
+            }
+          }
+          dynamic "base_condition_key" {
+            iterator = base_condition_key
+            for_each = { for k, v in conditions.value : k => v if k == "base_condition_key" }
+            content {
+              attribute      = base_condition_key.value.attribute
+              type           = base_condition_key.value.type
+            }
+          }
+          dynamic "bitness" {
+            iterator = bitness
+            for_each = { for k, v in conditions.value : k => v if k == "bitness" }
+            content {
+              operator      = bitness.value.operator
+              negate        = bitness.value.negate
+              value         = bitness.value.value
+            }
+          }
+          dynamic "bitness_comparision" {
+            iterator = bitness_comparision
+            for_each = { for k, v in conditions.value : k => v if k == "bitness_comparision" }
+            content {
+              operator      = bitness_comparision.value.operator
+              negate        = bitness_comparision.value.negate
+              type          = bitness_comparision.value.type
+              value         = bitness_comparision.value.value
+            }
+          }          
           dynamic "key" {
             iterator = key
             for_each = { for k, v in conditions.value : k => v if k == "key" }
